3a461e7ac7004548b1808f5b459733f50bdb236d
David <david17895@gmail.com>
2021-05-24 18:47:05 +0300

    concurrently setup

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
 import { join } from 'path';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(express.static(join(__dirname, '../uploads/')));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 5800);
 }
 bootstrap();
