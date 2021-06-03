42359a31bdd5ff8f8035ad2db42e6d91368504c3
David <david17895@gmail.com>
2021-06-03 11:42:39 +0300

    benefits styling

--- client/src/Home/Benefits.tsx
+++ client/src/Home/Benefits.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import document from '../media/document.svg';
-import { FaCog, FaUserClock, FaServer, FaBatteryEmpty } from 'react-icons/fa';
-import { IconType } from 'react-icons/lib';
+import { FaCog, FaUserClock, FaServer } from 'react-icons/fa';
+import styled from 'styled-components';
+import { colorsVariables } from '../GlobalStyle';
 interface Benefit {
   header: string;
   text: string;
@@ -11,7 +11,7 @@ interface Benefit {
 const Benefis: React.FC = () => {
   const benefits: Benefit[] = [
     {
-      header: 'Take advantage of Technology',
+      header: 'Take advantage of technology',
       text: 'Use a smart system to keep track of parcels and manage them',
       icon: <FaCog />,
     },
@@ -21,22 +21,62 @@ const Benefis: React.FC = () => {
       icon: <FaUserClock />,
     },
     {
-      header: 'Use servers, Not paperwork',
+      header: 'Use servers, not paperwork',
       text: 'No more paperwork, Our cloud servers will keep your workflow and data organized',
       icon: <FaServer />,
     },
   ];
   return (
-    <>
-      {benefits.map((benefit) => (
-        <>
-          {benefit.icon}
-          <h4>{benefit.header}</h4>
-          <p>{benefit.text}</p>
-        </>
-      ))}
-    </>
+    <StyledBenefits>
+      <h2>Why use our service?</h2>
+      <BenefitsWrapper>
+        {benefits.map((benefit) => (
+          <StyledBenefit>
+            {benefit.icon}
+            <h3>{benefit.header}</h3>
+            <p>{benefit.text}</p>
+          </StyledBenefit>
+        ))}
+      </BenefitsWrapper>
+    </StyledBenefits>
   );
 };
+
+const StyledBenefits = styled.div`
+  h2 {
+    margin-bottom: 16px;
+    color: ${colorsVariables.secondaryDark};
+    text-align: center;
+  }
+`;
+
+const BenefitsWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 2rem;
+  width: 100%;
+`;
+
+const StyledBenefit = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-basis: 300px;
+  flex-grow: 1;
+  align-items: center;
+  margin: 0 auto;
+  text-align: center;
+
+  h3 {
+    margin-bottom: 0.2rem;
+  }
+
+  svg {
+    color: ${colorsVariables.main};
+    margin-bottom: 0.5rem;
+    width: 3rem;
+    height: 3rem;
+  }
+`;
 
 export default Benefis;
