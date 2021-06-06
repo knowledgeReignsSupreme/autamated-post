b030ea301301649a95da9505593d28e41261430b
David <david17895@gmail.com>
2021-06-06 10:04:17 +0300

    apollo client init

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,16 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: 'http://localhost:3000/graphql',
+});
 
 ReactDOM.render(
   <React.StrictMode>
